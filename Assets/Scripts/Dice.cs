5d6fe8f29d3744b34745573298a88f2c8463d80b
Ryan Stillings <rystills@gmail.com>
2022-07-16 23:49:39 -0400

    draw cuts, validate cuts, determine num required cuts

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -1,13 +1,23 @@
 using EzySlice;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
     [SerializeField] Material fruitMat;
+    [SerializeField] GameObject lrPrefab;
     [SerializeField] Rigidbody rb;
+    LineRenderer lr;
     int numIdleFrames = 5;
+    bool readyToCut = false;
+    bool midCut = false;
+    List<int> sideVals = new List<int>
+    {
+        3,4,1,6,5,2
+    };
+    int targetCutCount;
 
     void Start()
     {
@@ -19,25 +29,71 @@ public class Dice : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            GameObject[] halves = gameObject.SliceInstantiate(transform.position, Vector3.up);
-            Destroy(gameObject);
-            MeshRenderer mr;
-            foreach (GameObject go in halves)
-            {
-                List<Material> mats = new List<Material>();
-                mr = go.GetComponent<MeshRenderer>();
-                mr.GetMaterials(mats);
-                mats[1] = fruitMat;
-                mr.materials = mats.ToArray();
-            }
-        }
+        // check idle time
         if (rb.velocity.magnitude < .1f)
         {
             if (--numIdleFrames == 0)
             {
-                Debug.Log("idle");
+                readyToCut = true;
+                List<Vector3> sideAngles = new List<Vector3>
+                {
+                    transform.up,
+                    -transform.up,
+                    transform.right,
+                    -transform.right,
+                    transform.forward,
+                    -transform.forward
+                };
+                targetCutCount = sideVals[sideAngles.IndexOf(sideAngles.Aggregate((a, b) => Vector3.Angle(a, Vector3.up) < Vector3.Angle(b, Vector3.up) ? a : b))];
+            }
+        }
+    }
+
+    private bool ValidateCut(Vector3 s, Vector3 e)
+    {
+        s.y -= 1.5f;
+        e.y -= 1.5f;
+        //Debug.DrawRay(s,(e-s),Color.blue,100);
+        return Physics.Raycast(new Ray(s, (e-s).normalized), (e-s).magnitude, LayerMask.GetMask("dice")) &&
+               Physics.Raycast(new Ray(e, (s-e).normalized), (s-e).magnitude, LayerMask.GetMask("dice"));
+    }
+
+    private void Update()
+    {
+        if (readyToCut)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                midCut = true;
+                lr = Instantiate(lrPrefab).GetComponent<LineRenderer>();
+                lr.positionCount += 2;
+                lr.SetPosition(0, Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 9));
+                lr.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 9));
+            }
+            else if (midCut)
+            {
+                lr.SetPosition(1, Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 9));
+                if (Input.GetMouseButtonUp(0)) {
+                    midCut = false;
+                    if (!ValidateCut(lr.transform.TransformPoint(lr.GetPosition(0)), lr.transform.TransformPoint(lr.GetPosition(1))))
+                    {
+                        Destroy(lr.gameObject);
+                    }
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                GameObject[] halves = gameObject.SliceInstantiate(transform.position, Vector3.up);
+                Destroy(gameObject);
+                MeshRenderer mr;
+                foreach (GameObject go in halves)
+                {
+                    List<Material> mats = new List<Material>();
+                    mr = go.GetComponent<MeshRenderer>();
+                    mr.GetMaterials(mats);
+                    mats[1] = fruitMat;
+                    mr.materials = mats.ToArray();
+                }
             }
         }
     }
